9cdd0c8ba91cda8e7f6842b2e60ec072caeacc8e
Metelity <kirillharin3144@yandex.ru>
2025-03-28 12:37:55 +0500

    Create __init__.py

---
+++ hegram/__init__.py
@@ -0,0 +1,10 @@
+from .bot import Bot
+from .types import ReplyKeyboardMarkup, ReplyKeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+
+__all__ = [
+    'Bot',
+    'ReplyKeyboardMarkup',
+    'ReplyKeyboardButton',
+    'InlineKeyboardButton',
+    'InlineKeyboardMarkup'
+]
